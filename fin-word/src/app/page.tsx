4874ac8f0dc60fc9c65018b92041019c5d347845
syamshukla <53063487+syamshukla@users.noreply.github.com>
2023-11-27 18:33:42 -0500

    table

--- fin-word/src/app/page.tsx
+++ fin-word/src/app/page.tsx
@@ -1,22 +1,23 @@
 "use client";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
-// export class page extends Component {
-//   render() {
-//     return (
-// <>
-//   <div className="grid w-full max-w-sm items-center gap-1.5">
-//     <Label htmlFor="email">Email</Label>
-//     <Input type="email" id="email" placeholder="Email" />
-//   </div>
-//   <div>FinWord</div>;
-// </>
-//     );
-//   }
-// }
-
-// export default page;
+const getStockData = async (symbol: any) => {
+  const response = await fetch(
+    `https://api.polygon.io/v3/reference/tickers/${symbol}?apiKey=${uyJZfjE33Dd1QXpRxp20ie5tZHdLx3lH}`
+  );
+  const data = await response.json();
+  return data;
+};
 
 import React from "react";
 
@@ -27,7 +28,36 @@ export default function page() {
         <Label htmlFor="email">Stock Ticker</Label>
         <Input type="stock" id="ticker" placeholder="AAPL" />
       </div>
-      
+      <Table className="">
+        <TableHeader>
+          <TableRow>
+            <TableHead>Ticker</TableHead>
+            <TableHead>Price</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell className="font-medium">1</TableCell>
+            <TableCell>1</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell className="font-medium">1</TableCell>
+            <TableCell>1</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell className="font-medium">1</TableCell>
+            <TableCell>1</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell className="font-medium">1</TableCell>
+            <TableCell>1</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell className="font-medium">1</TableCell>
+            <TableCell>1</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
     </>
   );
 }
