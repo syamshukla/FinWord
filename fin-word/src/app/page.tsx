a48c75df53c73cc7b8277d9affdf44cd8e0a07fa
Chris Abdo <chris.j.abdo@gmail.com>
2023-11-27 17:46:22 -0500

    Refactor page component

--- fin-word/src/app/page.tsx
+++ fin-word/src/app/page.tsx
@@ -1,19 +1,32 @@
 "use client";
-import React, { Component } from "react";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-export class page extends Component {
-  render() {
-    return (
-      <>
-        <div className="grid w-full max-w-sm items-center gap-1.5">
-          <Label htmlFor="email">Email</Label>
-          <Input type="email" id="email" placeholder="Email" />
-        </div>
-        <div>FinWord</div>;
-      </>
-    );
-  }
-}
+// export class page extends Component {
+//   render() {
+//     return (
+// <>
+//   <div className="grid w-full max-w-sm items-center gap-1.5">
+//     <Label htmlFor="email">Email</Label>
+//     <Input type="email" id="email" placeholder="Email" />
+//   </div>
+//   <div>FinWord</div>;
+// </>
+//     );
+//   }
+// }
 
-export default page;
+// export default page;
+
+import React from "react";
+
+export default function page() {
+  return (
+    <>
+      <div className="grid w-full max-w-sm items-center gap-1.5">
+        <Label htmlFor="email">Email</Label>
+        <Input type="email" id="email" placeholder="Email" />
+      </div>
+      <div>FinWord</div>;
+    </>
+  );
+}
