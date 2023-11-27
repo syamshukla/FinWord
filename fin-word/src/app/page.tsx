02b726f8d792b544b2baa8de9e83941759cfb3fa
syamshukla <53063487+syamshukla@users.noreply.github.com>
2023-11-27 17:43:28 -0500

    f

--- fin-word/src/app/page.tsx
+++ fin-word/src/app/page.tsx
@@ -1,9 +1,18 @@
 "use client";
 import React, { Component } from "react";
-
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 export class page extends Component {
   render() {
-    return <div>FinWord</div>;
+    return (
+      <>
+        <div className="grid w-full max-w-sm items-center gap-1.5">
+          <Label htmlFor="email">Email</Label>
+          <Input type="email" id="email" placeholder="Email" />
+        </div>
+        <div>FinWord</div>;
+      </>
+    );
   }
 }
 
