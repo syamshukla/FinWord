72d05618302dac356aef095b2ef1f906d40ff361
syamshukla <53063487+syamshukla@users.noreply.github.com>
2023-11-27 19:19:30 -0500

    logic

--- fin-word/src/app/page.tsx
+++ fin-word/src/app/page.tsx
@@ -1,6 +1,9 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 "use client";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+
+import React, { useState } from "react";
 import {
   Table,
   TableBody,
@@ -10,23 +13,67 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-
-const getStockData = async (symbol: any) => {
-  const response = await fetch(
-    `https://api.polygon.io/v3/reference/tickers/${symbol}?apiKey=uyJZfjE33Dd1QXpRxp20ie5tZHdLx3lH}`
-  );
-  const data = await response.json();
-  return data;
-};
-
-import React from "react";
+import { Button } from "@/components/ui/button";
 
 export default function page() {
+  const [ticker, setTicker] = useState("");
+  const tickers: never[] = [];
+
+  const saveReset = async () => {
+    const data = getStockData(ticker);
+    console.log(data);
+  };
+  function handleKeyPress(
+    e: React.KeyboardEvent<HTMLInputElement>,
+    ticker: string
+  ) {
+    var key = e.key;
+    console.log("You pressed a key: " + key);
+    if (key == "Enter") {
+      setTicker(e.currentTarget.value.toUpperCase());
+      tickers.push(e.currentTarget.value.toUpperCase());
+      saveReset();
+    }
+  }
+  const getStockData = async (symbol: any) => {
+    const apiKey = "uyJZfjE33Dd1QXpRxp20ie5tZHdLx3lH"; // Replace with your actual API key
+
+    try {
+      const response = await fetch(
+        `https://api.polygon.io/v3/reference/tickers/${symbol}`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${apiKey}`,
+            // You can include other headers if required, for example:
+            // 'Content-Type': 'application/json'
+          },
+          mode: "cors", // Include this line to enable CORS
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(`Error: ${response.statusText}`);
+      }
+
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("Error:", error);
+      throw error;
+    }
+  };
   return (
     <div className="h-screen flex flex-col justify-center items-center">
       <div className="grid w-full max-w-sm items-center gap-1.5">
         <Label htmlFor="email">Stock Ticker</Label>
-        <Input type="stock" id="ticker" placeholder="AAPL" />
+        <input
+          type="text"
+          id="ticker"
+          placeholder="AAPL"
+          onChange={(e) => setTicker(e.target.value.toUpperCase())}
+        />
+        <button onClick={saveReset}>Enter</button>
       </div>
       <Table className="">
         <TableHeader>
@@ -37,8 +84,8 @@ export default function page() {
         </TableHeader>
         <TableBody>
           <TableRow>
-            <TableCell className="font-medium">1</TableCell>
-            <TableCell>1</TableCell>
+            <TableCell className="font-medium"></TableCell>
+            <TableCell></TableCell>
           </TableRow>
           <TableRow>
             <TableCell className="font-medium">1</TableCell>
@@ -60,4 +107,7 @@ export default function page() {
       </Table>
     </div>
   );
+}
+function getStockData(ticker: string) {
+  throw new Error("Function not implemented.");
 }
