2c99b27e596cfe20643a3009f2d25b2a9b63c30d
syamshukla <53063487+syamshukla@users.noreply.github.com>
2023-11-29 23:42:15 -0500

    mobile updates

--- fin-word/src/components/login/helper.ts
+++ fin-word/src/components/login/helper.ts
@@ -3,23 +3,23 @@ import { addUserToFirestore } from "@/lib/firebase/useController";
 import {
   browserSessionPersistence,
   setPersistence,
-  signInWithPopup,
+  signInWithRedirect,
+  getRedirectResult,
 } from "firebase/auth";
 import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
-
 
 export const handleSignIn = (router: AppRouterInstance) => {
   setPersistence(auth, browserSessionPersistence)
     .then(() => {
-      signInWithPopup(auth, googleProvider)
-        .then((result) => {
-          const user = result?.user;
-          addUserToFirestore(user);
-          router.push("/");
-        })
-        .catch((error) => {
-          throw new Error(error.message);
-        });
+      signInWithRedirect(auth, googleProvider);
+      return getRedirectResult(auth);
+    })
+    .then((result) => {
+      const user = result?.user;
+      if (user) {
+        addUserToFirestore(user);
+        router.push("/");
+      }
     })
     .catch((error) => {
       throw new Error(error.message);
