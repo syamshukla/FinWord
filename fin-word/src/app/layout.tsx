660f21a42c55e79df15b0f5eb82ba781e02e8947
Chris Abdo <chris.j.abdo@gmail.com>
2023-11-30 13:44:11 -0500

    Fix dynamic import in layout.tsx

--- fin-word/src/app/layout.tsx
+++ fin-word/src/app/layout.tsx
@@ -2,12 +2,14 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { ThemeProvider } from '@/components/theme-provider'
-import MainNav from '@/components/layout/main-nav'
 import { Container } from '@/components/layout/container'
 import { Toaster } from 'sonner'
 import Footer from '@/components/layout/footer'
 import { GeistSans } from 'geist/font/sans'
-
+import dynamic from 'next/dynamic'
+const MainNav = dynamic(() => import('@/components/layout/main-nav'), {
+  ssr: false,
+})
 export const metadata: Metadata = {
   title: 'BULL-ISH',
   description: '',
