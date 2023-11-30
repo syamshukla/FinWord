1cf20407d43803f74ce3cb3daccd6349418f8d14
syamshukla <53063487+syamshukla@users.noreply.github.com>
2023-11-29 23:55:17 -0500

    new

--- fin-word/src/app/login/page.tsx
+++ fin-word/src/app/login/page.tsx
@@ -1,15 +1,15 @@
-import dynamic from 'next/dynamic';
+import dynamic from 'next/dynamic'
 
 const LoginButton = dynamic(() => import('@/components/login/Login'), {
   ssr: false, // This ensures the component is not included in SSR
-});
+})
 
 export default function Login() {
   return (
-    <main className="flex h-screen items-center justify-center bg-white">
+    <main className="justify-cente flex h-screen items-center">
       <div className="flex flex-col items-center gap-10">
         <LoginButton />
       </div>
     </main>
-  );
+  )
 }
---
+++ fin-word/src/app/results/page.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class page extends Component {
+  render() {
+    return (
+      <div>page</div>
+    )
+  }
+}
+
+export default page
