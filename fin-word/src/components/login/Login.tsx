87c8fecee6e6d16fc465e33a916625835a6618ec
syamshukla <53063487+syamshukla@users.noreply.github.com>
2023-11-29 22:58:27 -0500

    new button

--- fin-word/src/components/login/Login.tsx
+++ fin-word/src/components/login/Login.tsx
@@ -3,16 +3,17 @@
 import { useRouter } from 'next/navigation'
 import React from 'react'
 import { handleSignIn } from './helper'
+import { Button } from '../ui/button'
 
 const LoginButton: React.FC = () => {
   const router = useRouter()
   return (
-    <button
+    <Button
       className="border-black-rounded rounded-md border-2 p-4 hover:bg-gray-100"
       onClick={() => handleSignIn(router)}
     >
       Sign In With Google
-    </button>
+    </Button>
   )
 }
 
